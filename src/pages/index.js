e262be6e17bffce29fb5933a6754f32453db0fb0
Fansong Zeng <fansong.cfs@alibaba-inc.com>
2022-03-31 16:44:04 +0800

    add Getting Started Link in home page (#21)
    * add Getting Started Link in home page
    
    Signed-off-by: Fansong Zeng <fanster.z@gmail.com>
    
    * fix style of button
    
    Signed-off-by: Fansong Zeng <fanster.z@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
-import Translate, { translate } from '@docusaurus/Translate';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Translate from '@docusaurus/Translate';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import React from 'react';
 import GitHubButton from 'react-github-btn';
 import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
 
 const Button = ({ children, href }) => {
   return (
     <div className="col col--2 margin-horiz--sm">
       <Link
-        className="button button--outline button--primary button--lg"
+        className="button button--outline"
         to={href}>
         {children}
       </Link>
@@ -36,16 +37,26 @@ function HomepageHeader() {
             Star
           </GitHubButton>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+
+        <div
+            className={clsx(styles.heroButtons, 'name', 'margin-vert--md')}>
+            <Button href={useBaseUrl('docs/installation')}><Translate>Getting Started</Translate> </Button>
+            <Button href={useBaseUrl('docs/')}><Translate>Learn More</Translate></Button>
+         </div>
+         
       </div>
     </header>
+
   );
 }
+
+
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`QoS based scheduling system`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
