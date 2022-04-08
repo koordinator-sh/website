21752e8d740928e58359138b0b18773d3819c98c
Fansong Zeng <fanster.z@gmail.com>
2022-04-08 11:43:52 +0800

    fix favicon
    Signed-off-by: Fansong Zeng <fanster.z@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico', // TODO(FillZpp)
+  favicon: 'img/favicon.ico',
   organizationName: 'koordinator-sh', // Usually your GitHub org/user name.
   projectName: 'koordinator.sh', // Usually your repo name.
 
