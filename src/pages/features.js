65b3ce08403c84f0c7a32e07624b002b277bfa4a
Fansong Zeng <fanster.z@gmail.com>
2022-04-29 18:13:10 +0800

    add what-koordinator-is section in homepage
    Signed-off-by: Fansong Zeng <fanster.z@gmail.com>

--- src/pages/features.js
+++ src/pages/features.js
@@ -1,8 +1,6 @@
-import clsx from 'clsx';
 import React from 'react';
-import styles from './styles.module.css';
 
-const FeatureList = [
+const features = [
   {
     title: '',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
@@ -20,6 +18,7 @@ const FeatureList = [
         Allowing for resource overcommitments to achieve high resource utilizations and ensure QoS guarantees by leveraging an application profiling mechanism.
       </>
     ),
+    reverse: true,
   },
   {
     title: '',
@@ -32,30 +31,5 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
 
-export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-}
+export default features
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,30 @@ import Link from '@docusaurus/Link';
 import Translate from '@docusaurus/Translate';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
 import React from 'react';
 import GitHubButton from 'react-github-btn';
+import features from './features';
 import styles from './index.module.css';
 
+function Feature({Svg, title, description, reverse}) {
+  return (
+    <div className={clsx('row', styles.feature, reverse ? styles.featureReverse : '')}>
+      <div className="col col--3">
+        <div className="text--center">
+          <Svg className={styles.featureSvg} role="img" />
+        </div>
+      </div>
+      <div className={clsx('col col--9', styles.featureDesc)}>
+        <div>
+          <h2>{title}</h2>
+          <div>{description}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 const Button = ({ children, href }) => {
   return (
@@ -54,7 +71,6 @@ function HomepageHeader() {
 }
 
 
-
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -62,9 +78,56 @@ export default function Home() {
       title={`QoS based scheduling system`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+      
+      <WhatIs />
+
+      <main className={clsx('hero', styles.hero)}>
+        <div className="container">
+          <section className={styles.features}>
+            <div className="container">
+              {features.map((props, idx) => (
+                <Feature key={idx} {...props} />
+              ))}
+            </div>
+          </section>
+        </div>
       </main>
     </Layout>
   );
 }
+
+
+
+const WhatIs = () => (
+  <div className={clsx('hero', styles.hero)}>
+    <div className="container">
+      <div className="row">
+        <div className="col col--6">
+          <h1><Translate>What is Koordinator?</Translate></h1>
+          <p className="hero__subtitle">
+            <small>
+              <Translate>
+                Koordinator is a modern scheduling system that colocate different types of workloads on kubernetes. 
+                It achieves high utilization by combining elastic resource quota, efficient pod-packing, over-commitment, and node sharing with container resource isolation.
+              </Translate>
+              <br />
+              <br />
+              <Translate>
+                Koordinator is high performance, scalable, yet most importantly,
+              </Translate><i><b> <Translate>proven in mass production environments.</Translate></b></i>
+              <Translate> It allows you to build container orchestration systems that support enterprise production environments.</Translate>
+            </small>
+          </p>
+        </div>
+        <div className="col">
+          <img
+            className="image"
+            src={useBaseUrl("img/what-is-koordinator.svg")}
+            align="right"
+            alt="what is koordinator"
+          />
+        </div>
+      </div>
+    </div>
+  </div >
+);
