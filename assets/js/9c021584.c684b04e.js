fd01746e77aa995a600935ff76690f0d1fbe0d2a
gh-actions <actions@github.com>
2022-07-01 10:39:56 +0000

    Deploy website - based on e4868e442c218c1750092dbd94397e42692c6bb2

---
+++ assets/js/9c021584.c684b04e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkoordinator_sh=self.webpackChunkkoordinator_sh||[]).push([[7438],{8055:function(e){e.exports=JSON.parse('{"permalink":"/blog/tags/release","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
