5b545e5ace4e363a8eb01bcb1650b7e173618584
gh-actions <actions@github.com>
2024-06-19 05:49:47 +0000

    Deploy website - based on f8d81e793b0c9f9dadf43de71dfecfec5ec9227a

--- zh-Hans/assets/js/4ff0fe2c.11339fa7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkoordinator_sh=self.webpackChunkkoordinator_sh||[]).push([[2188],{831:a=>{a.exports=JSON.parse('{"label":"release","permalink":"/zh-Hans/blog/tags/release","allTagsPath":"/zh-Hans/blog/tags","count":12}')}}]);
---
+++ zh-Hans/assets/js/74720335.d4788329.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkoordinator_sh=self.webpackChunkkoordinator_sh||[]).push([[6795],{3253:o=>{o.exports=JSON.parse('{"permalink":"/zh-Hans/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":14,"previousPage":"/zh-Hans/blog","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
